8ad58ace0756e2ffec2ed894ccb51b1a5fcce1fb
af-olivares10 <af.olivares10@uniandes.edu.co>
2017-09-24 13:36:20 -0500

    Update User.js

--- app/models/User.js
+++ app/models/User.js
@@ -2,7 +2,6 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 var bcrypt 		 = require('bcrypt-nodejs');
 
-// user schema
 var UserSchema   = new Schema({
 	name: String,
 	rol: String,
@@ -10,24 +9,19 @@ var UserSchema   = new Schema({
 	password: { type: String, required: true, select: false }
 });
 
-// hash the password before the user is saved
 UserSchema.pre('save', function(next) {
 	var user = this;
 
-	// hash the password only if the password has been changed or user is new
 	if (!user.isModified('password')) return next();
 
-	// generate the hash
 	bcrypt.hash(user.password, null, null, function(err, hash) {
 		if (err) return next(err);
 
-		// change the password to the hashed version
 		user.password = hash;
 		next();
 	});
 });
 
-// method to compare a given password with the database hash
 UserSchema.methods.comparePassword = function(password) {
 	var user = this;
 
