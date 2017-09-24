db02e49dd8852acb86dfc4f4cbfdd15fd3d9411d
jfmendez11 <jf.mendez11@uniandes.edu.co>
2017-09-24 14:12:21 -0500

    cambio de base de datos local a remota

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://localhost:27017/pruebaIntegrado',
+	'database': 'mongodb://ajapapi:5enarquisoft@ds149144.mlab.com:49144/mina_samaca_g4',
 	'secret': 'ilovescotchscotchyscotchscotch'
 };
