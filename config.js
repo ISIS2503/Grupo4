88e5d2b802e54ba8bdae3439a7e6449f88c67af9
ANDRES FELIPE OLIVARES VARGAS <af.olivares10@sis.virtual.uniandes.edu.co>
2017-10-23 10:35:21 -0500

    Cambio de maquina bd

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://ajapapi:5enarquisoft@ds149144.mlab.com:49144/mina_samaca_g4',
+	'database': 'mongodb://172.24.42.57:27017/myDataBase',
 	'secret': 'ilovescotchscotchyscotchscotch'
 };
